1fc5d90a2af497b5e1521c7c4891d15ef5551707
CintiaJimenez <cintiajimenez7@gmail.com>
2023-05-05 07:47:40 -0300

    crea el service experiencia  create delete update

---
+++ proyecto_integrador_cintia_jimenez/FrontEnd/src/app/model/experiencia.ts
@@ -0,0 +1,14 @@
+//replicsmos entity//
+
+export class Experiencia {
+
+    id?: number;
+    nombreE: string;
+    descripcionE: string;
+
+    contructor(nombreE: string, descripcionE: string) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+
+    }
+}
---
+++ proyecto_integrador_cintia_jimenez/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Experiencia } from '../model/experiencia';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SExperienciaService {
+
+    expURl = 'http://localhost:8080//explab'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public lista(): Observable<Experiencia[]>{
+    return this.httpClient.get<Experiencia[]>(this.expURl + 'lista');
+      }
+
+  public detail(id: number): Observable<Experiencia>{
+    return this.httpClient.get<Experiencia>(this.expURl + `detail/${id}`);
+  }
+
+  public save(experiencia: Experiencia): Observable<any>{
+    return this.httpClient.post<any>(this.expURl + 'create', experiencia);
+  }
+
+  public update (id: number, experiencia: Experiencia): Observable<any>{
+    return this.httpClient.put<any>(this.expURl + `update/${id}`, experiencia);
+  }
+
+  public delete (id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.expURl + `delete/${id}`);
+  }
+
+
+}
