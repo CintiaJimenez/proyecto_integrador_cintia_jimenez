c85ffee980e47848f498e7fba9093d331bb4d625
CintiaJimenez <cintiajimenez7@gmail.com>
2023-05-06 17:08:14 -0300

    crea lista erducacion

---
+++ proyecto_integrador_cintia_jimenez/FrontEnd/src/app/service/educacion.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Educacion } from '../model/educacion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EducacionService {
+  URL= 'http://localhost:8080/educacion/';
+
+  constructor(private httpClient : HttpClient) { }
+
+  public lista(): Observable<Educacion[]>{
+    return this.httpClient.get<Educacion[]>(this.URL + 'lista');
+  }
+
+
+}
